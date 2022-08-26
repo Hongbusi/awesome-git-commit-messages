dca8fcfe7e6f8ffe1848796aa9faf22fd268b6db
Hongbusi <coderhbs@gmail.com>
2022-08-26 16:59:54 +0800

    feat: layout

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { toggleDark } from '~/composables'
+</script>
+
+<template>
+  <header class="border-b border-gray-200 dark:border-gray-800">
+    <nav class="flex justify-between items-center container px-4 mx-auto h-12">
+      <h2 class="text-xl font-600">
+        Commit Example
+      </h2>
+      <div class="text-xl">
+        <button class="icon-btn mx-2 !outline-none" title="切换深色模式" @click="toggleDark()">
+          <div i="carbon-sun dark:carbon-moon" />
+        </button>
+
+        <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/Hongbusi/commit-example" target="_blank" title="GitHub">
+          <div i-carbon-logo-github />
+        </a>
+      </div>
+    </nav>
+  </header>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <router-view />
-    <Footer />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Default Layout]
+  <main class="text-gray-700 dark:text-gray-200">
+    <Header />
+    <div class="container px-4 mx-auto">
+      <router-view />
     </div>
   </main>
 </template>
