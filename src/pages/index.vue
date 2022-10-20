cdecb279d151299c318438f153e189f57590599f
Hongbusi <coderhbs@gmail.com>
2022-10-20 15:42:29 +0800

    chore: hero button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,14 +3,12 @@ import config from '~/config'
 </script>
 
 <template>
-  <div class="mx-auto my-12 w-3/4 text-center">
-    <h1 class="text-5xl font-700 leading-tight">
-      Git commit message example.
+  <div class="mx-auto my-12 text-center">
+    <h1 class="mb-5 text-5xl font-700 leading-tight">
+      A collection of Git commit message examples to make your project's commit history more readable.
     </h1>
 
-    <button class="btn mt-5">
-      Star me on GitHub ● 6248★
-    </button>
+    <HeroButton repo="git-commit-message-example" />
   </div>
 
   <List :list="config.commitList" />
